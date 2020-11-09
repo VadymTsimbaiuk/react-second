f905d34b9d4576bc4db72f70b84a6e13ddf2d63f
Vadym Tcimbaliyk <vadymonua@gmail.com>
2020-11-09 09:06:32 +0200

    1_stage

---
+++ build/precache-manifest.f6d0f06e6cbdc83884421740c0ce45f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d114879ae1c91daf5cb4f769a68932",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e13160223f328dcd45e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a9ceda0130410752228f",
+    "url": "/static/js/2.c6a1576e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c6a1576e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e13160223f328dcd45e",
+    "url": "/static/js/main.db9469b5.chunk.js"
+  },
+  {
+    "revision": "6791ee339c15ed7a9973",
+    "url": "/static/js/runtime-main.a6b9953f.js"
+  }
+]);
